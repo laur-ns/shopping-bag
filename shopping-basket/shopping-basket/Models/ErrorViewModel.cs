5ab36cd30aa29402fa268590282ca5b9f368ebb3
LAURENS <80611334+laur-ns@users.noreply.github.com>
2022-01-26 23:21:42 +1300

    Init asp.net

---
+++ shopping-basket/shopping-basket/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shopping_basket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
