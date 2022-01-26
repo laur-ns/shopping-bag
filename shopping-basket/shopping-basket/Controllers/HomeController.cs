5373e85bc7f11b9c86dffc769c2dcd665c69e1bb
LAURENS <80611334+laur-ns@users.noreply.github.com>
2022-01-27 11:53:04 +1300

    Reset corrupt files

--- shopping-basket/shopping-basket/Controllers/HomeController.cs
+++ shopping-basket/shopping-basket/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using shopping_basket.Models;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace shopping_basket.Controllers
 {
--- shopping-basket/shopping-basket/Models/ErrorViewModel.cs
+++ shopping-basket/shopping-basket/Models/ErrorViewModel.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace shopping_basket.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
