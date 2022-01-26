2e2f6ba05b830f3f875f418c09e71d72ef17d949
LAURENS <80611334+laur-ns@users.noreply.github.com>
2022-01-26 23:42:00 +1300

    Add Actor model

---
+++ shopping-basket/shopping-basket/Models/actor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shopping_basket.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Profile_Pic { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+    }
+}
