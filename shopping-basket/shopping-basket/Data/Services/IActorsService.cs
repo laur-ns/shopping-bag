e74fcdcfe0a22c1b6b4daa232de6df5057ab5bb9
LAURENS <80611334+laur-ns@users.noreply.github.com>
2022-01-31 21:39:14 +1300

    Add actors interface

---
+++ shopping-basket/shopping-basket/Data/Services/IActorsService.cs
@@ -0,0 +1,17 @@
+﻿using shopping_basket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shopping_basket.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
